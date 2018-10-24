06fcb94d91e3f71beb145e9c371e757b62fa6b7a
filippo.ermini <filippo.ermini@gmail.com>
2018-10-24 23:34:05 +0200

    modifica del modello

--- main.py
+++ main.py
@@ -91,14 +91,18 @@ y_test  = dataset[3]
 input_shape = (image_width, image_height, 4)
 
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1),activation='relu',input_shape=input_shape))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-model.add(Conv2D(64, (5, 5), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Flatten())
-model.add(Dense(1000, activation='relu'))
-model.add(Dense(outClass,activation='softmax'))
-model.compile(loss=keras.losses.categorical_crossentropy,optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+model.add(Dense(64, activation='relu',input_shape=(image_width, image_height, 4)))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(1))
+model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+
+
+#model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1),activation='relu',input_shape=input_shape))
+#model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+#model.add(Conv2D(64, (5, 5), activation='relu'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+#model.add(Flatten())
+#model.add(Dense(1000, activation='relu'))
 
 class AccuracyHistory(keras.callbacks.Callback):
     def on_train_begin(self, logs={}):
